df36dae660073b85ec2b246ae45d0df85b880279
Santuario Parra Luis Fernando <36172678+SantuarioParra@users.noreply.github.com>
2018-04-10 20:23:20 -0500

    Initial commit

---
+++ src/Chat/Chat.java
@@ -0,0 +1,18 @@
+package Chat;
+
+import javax.swing.*;
+
+public class Chat {
+    private JPanel Principal;
+    private JEditorPane editorPane1;
+    private JList list1;
+    private JTextField textField1;
+    private JButton enviarButton;
+    private JButton añadirImageButton;
+    private JButton button3;
+    private JButton button4;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JButton button8;
+}
